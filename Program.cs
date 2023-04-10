aa19c8ed614520a2b9c4c574c310c0b4c7e21555
Tomo4kap <tomo4kap@gmail.ru>
2023-04-10 16:50:40 +0300

    min-max-final vercion

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Max = {number1} Min = {number2}");
+}
+else
+{
+    Console.WriteLine($"Max = {number2} Min = {number1}");
+}
